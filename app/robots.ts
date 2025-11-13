5065437dcde441ce921e393153ca252489c702a8
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-11-13 18:18:00 +0900

    feat: add sitemap log

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+﻿import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private/',
+    },
+    sitemap: 'https://zziglet.com/sitemap.xml',
+  };
+}
