ae739ece3a56a2e6cb6ade9a4c1ee0495dcb053d
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-11-13 18:30:45 +0900

    chore: add default folder structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/icon?<generated>" type="image/<generated>" sizes="<generated>" />
+        <link rel="icon" href="./icon.svg" type="image/<generated>" sizes="<generated>" />
       </head>
       <body>{children}</body>
     </html>
