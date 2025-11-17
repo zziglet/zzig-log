d3c881440263f7307feed817439f60688717a8a1
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-11-17 16:36:42 +0900

    fix: change fonts directory path

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -3,47 +3,47 @@
 export const suit = localFont({
   src: [
     {
-      path: '../public/fonts/suit/SUIT-Thin.woff2',
+      path: '../../public/fonts/suit/SUIT-Thin.woff2',
       weight: '100',
       style: 'normal',
     },
     {
-      path: '../public/fonts/suit/SUIT-ExtraLight.woff2',
+      path: '../../public/fonts/suit/SUIT-ExtraLight.woff2',
       weight: '200',
       style: 'normal',
     },
     {
-      path: '../public/fonts/suit/SUIT-Light.woff2',
+      path: '../../public/fonts/suit/SUIT-Light.woff2',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '../public/fonts/suit/SUIT-Regular.woff2',
+      path: '../../public/fonts/suit/SUIT-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../public/fonts/suit/SUIT-Medium.woff2',
+      path: '../../public/fonts/suit/SUIT-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../public/fonts/suit/SUIT-SemiBold.woff2',
+      path: '../../public/fonts/suit/SUIT-SemiBold.woff2',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '../public/fonts/suit/SUIT-Bold.woff2',
+      path: '../../public/fonts/suit/SUIT-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
     {
-      path: '../public/fonts/suit/SUIT-ExtraBold.woff2',
+      path: '../../public/fonts/suit/SUIT-ExtraBold.woff2',
       weight: '800',
       style: 'normal',
     },
     {
-      path: '../public/fonts/suit/SUIT-Heavy.woff2',
+      path: '../../public/fonts/suit/SUIT-Heavy.woff2',
       weight: '900',
       style: 'normal',
     },
