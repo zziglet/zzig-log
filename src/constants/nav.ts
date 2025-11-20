8d19233b0927323a792fc8676d728448765c37a2
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-11-21 02:52:09 +0900

    feat: add nav constants data

---
+++ src/constants/nav.ts
@@ -0,0 +1,14 @@
+﻿export const NAV_LINKS = [
+  {
+    title: 'Info',
+    href: '/info',
+  },
+  {
+    title: 'Blog',
+    href: '/blog',
+  },
+  {
+    title: 'Portfolio',
+    href: '/portfolio',
+  },
+];
