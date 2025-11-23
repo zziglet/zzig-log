4080865cab12b59e0ccf33233eca23eeeafc0a62
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-11-24 04:37:00 +0900

    chore: add info assets

---
+++ src/constants/info.ts
@@ -0,0 +1,23 @@
+﻿export const INFO_IMAGES = {
+  homeIcon1: '/images/home_1.png',
+  homeIcon2: '/images/home_2.png',
+};
+
+export const PERSONAL_INFO = {
+  email: 'zziglet0403@gmail.com',
+  github: 'zziglet',
+  education: 'Konkuk University Computer Science(B.S.)',
+};
+
+export const CAREER_HISTORY = [
+  { period: '2024.09 ~ 2025.06', role: 'GDGoC Konkuk 24-25 season Member' },
+  { period: '2025.06 ~ 2025.08', role: '건국대학교 학생개발팀 4기 Web Developer' },
+  { period: '2025.04 ~ ing', role: '인와이즈 Front-end Developer' },
+  { period: '2025.07 ~ ing', role: 'GDGoC Konkuk 25-26 season Core' },
+  { period: '2025.09 ~ ing', role: 'KioSchool Front-end Developer' },
+];
+
+export const LINKS = {
+  blog: '/blog',
+  portfolio: '/portfolio',
+};
