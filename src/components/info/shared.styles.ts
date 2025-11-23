136dd58d9cd1c8697b886bc468644c004a31b1f0
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-11-24 04:41:57 +0900

    style: add common info page styles

---
+++ src/components/info/shared.styles.ts
@@ -0,0 +1,32 @@
+﻿import { theme } from '@/styles/theme';
+import styled from '@emotion/styled';
+
+export const SectionContainer = styled.section<{ bgColor?: string }>`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: ${theme.colors.background.base};
+
+  & > div {
+    width: 100%;
+  }
+`;
+
+export const SectionTitle = styled.div`
+  font-size: ${theme.textSizes.heading['4xl']};
+  font-weight: 200;
+  color: ${theme.colors.cream[600]};
+  line-height: 1;
+  padding: 52px 0;
+
+  @media (max-width: 768px) {
+    font-size: ${theme.textSizes.heading['2xl']};
+  }
+`;
+
+export const HighlightText = styled.span`
+  color: ${theme.colors.peach[600]};
+  font-weight: bold;
+`;
