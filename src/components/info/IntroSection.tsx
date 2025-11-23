a107cb5b6cb5d0bfd231b015e8dda530fa4e0192
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-11-24 04:42:17 +0900

    style: add intro section component

---
+++ src/components/info/IntroSection.tsx
@@ -0,0 +1,59 @@
+﻿import styled from '@emotion/styled';
+import { SectionContainer, SectionTitle } from './shared.styles';
+import { INFO_IMAGES } from '@/constants/info';
+import { theme } from '@/styles/theme';
+
+const DescriptionBox = styled.div`
+  text-align: left;
+  padding-bottom: 32px;
+  word-break: keep-all;
+  overflow-wrap: normal;
+`;
+
+const Greeting = styled.div`
+  font-size: ${theme.textSizes.body.sm};
+  color: ${theme.colors.text.body};
+  font-weight: bold;
+
+  @media (max-width: 768px) {
+    font-size: 16px;
+  }
+`;
+
+const HighlightName = styled.span`
+  color: ${theme.colors.cream[600]};
+`;
+
+const SubTitle = styled.div`
+  font-size: ${theme.textSizes.body.xs};
+  color: ${theme.colors.peach[600]};
+  font-weight: 500;
+  padding-top: 8px;
+
+  @media (max-width: 768px) {
+    font-size: 12px;
+  }
+`;
+
+const MainImage = styled.img`
+  width: 100%;
+  max-height: 600px;
+  object-fit: cover;
+`;
+
+function IntroSection() {
+  return (
+    <SectionContainer>
+      <DescriptionBox>
+        <SectionTitle>zziglet</SectionTitle>
+        <Greeting>
+          안녕하세요, 끊임 없이 전진하고 싶은 개발자, <HighlightName>정지원</HighlightName>입니다.
+        </Greeting>
+        <SubTitle>Web Front-End Developer in Seoul, Korea</SubTitle>
+      </DescriptionBox>
+      <MainImage src={INFO_IMAGES.homeIcon1} alt="Intro Main" />
+    </SectionContainer>
+  );
+}
+
+export default IntroSection;
