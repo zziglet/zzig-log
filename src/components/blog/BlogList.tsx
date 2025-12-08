4c7e03ea9bdb987ec03995dd61b0da3f48e2fa45
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-12-08 14:15:16 +0900

    feat: add BlogList component

---
+++ src/components/blog/BlogList.tsx
@@ -0,0 +1,43 @@
+﻿'use client';
+
+import styled from '@emotion/styled';
+import { BlogPost } from '@/types/blog';
+import { theme } from '@/styles/theme';
+import BlogItem from './BlogItem';
+
+const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 8px 0 52px 0;
+  gap: 24px;
+`;
+
+const EmptyState = styled.div`
+  width: 100%;
+  padding: 100px 0;
+  text-align: center;
+  color: ${theme.colors.text.disabled};
+  font-size: 16px;
+`;
+
+interface BlogListProps {
+  posts: BlogPost[];
+  onClick: (post: BlogPost) => void;
+}
+
+function BlogList({ posts, onClick }: BlogListProps) {
+  if (!posts || posts.length === 0) {
+    return <EmptyState>게시물이 존재하지 않습니다.</EmptyState>;
+  }
+
+  return (
+    <ListContainer>
+      {posts.map((post) => (
+        <BlogItem key={post.id} post={post} onClick={() => onClick(post)} />
+      ))}
+    </ListContainer>
+  );
+}
+
+export default BlogList;
