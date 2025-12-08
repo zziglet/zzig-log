1d7861c860aae011e85839d2fec13debf8b39948
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-12-08 14:26:40 +0900

    style: edit fontsize of EmptyState component

--- src/components/blog/BlogList.tsx
+++ src/components/blog/BlogList.tsx
@@ -18,7 +18,7 @@ const EmptyState = styled.div`
   padding: 100px 0;
   text-align: center;
   color: ${theme.colors.text.disabled};
-  font-size: 16px;
+  font-size: ${theme.textSizes.body.md};
 `;
 
 interface BlogListProps {
