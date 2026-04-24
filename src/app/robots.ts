2319e66fae296a5b6852c008a04d99710730872d
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2026-04-24 14:36:36 +0900

    refactor: add list page metadata and robots url

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,4 +1,5 @@
 ﻿import type { MetadataRoute } from 'next';
+import { BASE_URL } from '@/constants';
 
 export default function robots(): MetadataRoute.Robots {
   return {
@@ -7,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://zziglet.com/sitemap.xml',
+    sitemap: `${BASE_URL}/sitemap.xml`,
   };
 }
